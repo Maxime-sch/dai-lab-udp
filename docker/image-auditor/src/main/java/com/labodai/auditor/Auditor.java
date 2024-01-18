a31b1cd43d92a0a93a61227b576b1cae33293967
ansermgw <25158748+ansermgw@users.noreply.github.com>
2024-01-18 14:11:59 +0100

    add module with shared code

---
+++ docker/image-auditor/src/main/java/com/labodai/auditor/Auditor.java
@@ -0,0 +1,14 @@
+package com.labodai.auditor;
+
+import com.labodai.shared.Instrument;
+
+class Auditor {
+    public static void main(String[] args) {
+        Instrument test;
+        // 1. Create a TCP server listening on port 2205
+
+        // 2. Create a UDP client listening for message from 239.255.22.5:9904
+
+        // 3.
+    }
+}
---
+++ docker/shared-code/src/main/java/com/labodai/shared/Instrument.java
@@ -0,0 +1,5 @@
+package com.labodai.shared;
+
+public enum Instrument {
+    GUITAR, BASS, BANJO, DOBRO, FIDDLE, SITAR
+}
