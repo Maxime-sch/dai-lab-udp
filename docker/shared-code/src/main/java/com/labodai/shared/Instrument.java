bf23d077f5b67d23a9ab319c3e0352a79a648110
Maxime-sch <55839375+Maxime-sch@users.noreply.github.com>
2024-01-19 16:42:17 +0100

    Implement getSound method for instruments

--- docker/shared-code/src/main/java/com/labodai/shared/Instrument.java
+++ docker/shared-code/src/main/java/com/labodai/shared/Instrument.java
@@ -5,6 +5,30 @@ public enum Instrument {
 
     private final String sound;
 
+    public String getSound() {
+        
+        switch(this) {
+      
+      case PIANO:
+        return "ti-ta-ti";
+
+      case TRUMPET:
+        return "pouet";
+
+      case FLUTE:
+        return "trulu";
+
+      case VIOLIN:
+        return "gzi-gzi";
+      
+      case DRUM:
+        return "boum-boum";
+
+      default:
+        return null;
+      }
+    }
+
     public static void convert(String value){
         Instrument.valueOf(value);
     }
