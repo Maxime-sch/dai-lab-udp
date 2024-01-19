26da9862508a9cf7f74032e991aaccbc732f31e8
Maxime-sch <55839375+Maxime-sch@users.noreply.github.com>
2024-01-19 17:07:31 +0100

    First implementation for periodic musician UDP communication

--- docker/image-musician/src/main/java/com/labodai/musician/Musician.java
+++ docker/image-musician/src/main/java/com/labodai/musician/Musician.java
@@ -7,41 +7,40 @@ import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetSocketAddress;
+import java.util.Timer;
+import java.util.TimerTask;
 import static java.nio.charset.StandardCharsets.*;
 
-class Musician{
-    Instrument sound;
-    String uuid;
+class Musician {
+    private Instrument instrument;
+    private String uuid;
 
-    public Musician(Instrument sound){
-        this.sound = sound;
+    public Musician(Instrument instrument) {
+        this.instrument = instrument;
         uuid = ""; //TODO
     }
 
-    public void playInstrument(){
+   public String toString() { 
+      return "{\"uuid\": \"" + uuid + "\", \"sound\": \" " + instrument.getSound() + "\"}"; 
+   }  
 
+    class PlayInstrument extends TimerTask {
+        public void run() {
+            try (DatagramSocket socket = new DatagramSocket()) {
+                  String message = this.toString();
+                  byte[] payload = message.getBytes(UTF_8);
+                  InetSocketAddress dest_address = new InetSocketAddress(UdpConstants.IP, UdpConstants.PORT);
+                  var packet = new DatagramPacket(payload, payload.length, dest_address);
+                  socket.send(packet);
+             } catch (IOException ex) {
+                 System.out.println(ex.getMessage());
+             }        
+        }
     }
 
     public static void main(String[] args) {
         Instrument test;
+        Timer timer = new Timer();
+        timer.schedule(new PlayInstrument(), 0, 1000);
     }
 }
-
-/*
-class MulticastSender {
-    final static String IPADDRESS = UdpConstants.;
-    final static int PORT = 44444;
-
-    public static void main(String[] args) {
-        try (DatagramSocket socket = new DatagramSocket()) {
-
-            String message = "Hello group members!";
-            byte[] payload = message.getBytes(UTF_8);
-            InetSocketAddress dest_address = new InetSocketAddress(IPADDRESS, 44444);
-            var packet = new DatagramPacket(payload, payload.length, dest_address);
-            socket.send(packet);
-        } catch (IOException ex) {
-            System.out.println(ex.getMessage());
-        }
-    }
-}*/
