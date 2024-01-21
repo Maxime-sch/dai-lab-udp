2b1c0d0ace0e2f96acde9bcf624fc859548e362f
Maxime-sch <55839375+Maxime-sch@users.noreply.github.com>
2024-01-21 16:03:12 +0100

    Fix TODOs in Musician.java

--- docker/image-musician/src/main/java/com/labodai/musician/Musician.java
+++ docker/image-musician/src/main/java/com/labodai/musician/Musician.java
@@ -4,9 +4,11 @@ import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetSocketAddress;
-import java.util.Timer;
 import java.util.TimerTask;
 import java.util.UUID;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import com.google.gson.Gson;
 
 import static java.nio.charset.StandardCharsets.*;
 
@@ -19,50 +21,62 @@ class Musician {
         uuid = UUID.randomUUID();
     }
 
-    // TODO burk, see https://studytrails.com/2016/09/12/java-google-json-parse-json-to-java/
-   public String toString() { // JSON serialization of our musician playing a sound
-      return "{\"uuid\": \"" + uuid.toString() + "\", \"sound\": \" " + instrument.getSound() + "\"}";
-   }  
+    static class MusicianInfo { // this inner class contains the information we want to serialize using GSON
+        private final UUID uuid;
+        private final String sound;
 
-    class PlayInstrument extends TimerTask { //sends UDP packets with the aforementioned serialization
+        public MusicianInfo(UUID uuid, String sound) {
+            this.uuid = uuid;
+            this.sound = sound;
+        }
+    }
+
+    public String toString() { // JSON serialization of our musician playing a sound
+        MusicianInfo musicianInfo = new MusicianInfo(uuid, instrument.getSound());
+        Gson gson = new Gson();
+        return gson.toJson(musicianInfo);
+    }
+
+    class PlayInstrument extends TimerTask { // inner class to send UDP packets with the serialization defined in toString
+        private final DatagramSocket socket;
+        private final DatagramPacket packet;
+        private final String message;
+
+        public PlayInstrument() throws IOException {
+            socket = new DatagramSocket();
+            message = Musician.this.toString();
+            byte[] payload = message.getBytes(UTF_8);
+            InetSocketAddress dest_address = new InetSocketAddress("239.255.22.5", 9904);
+            packet = new DatagramPacket(payload, payload.length, dest_address);
+        }
         public void run() {
-            // TODO inefficient to open a new socket everytime you send a message, please fix
-            // also you can use the same datagrame everytime
-            try (DatagramSocket socket = new DatagramSocket()) {
-                  String message = Musician.this.toString();
-                  byte[] payload = message.getBytes(UTF_8);
-                  InetSocketAddress dest_address = new InetSocketAddress("239.255.22.5", 9904);
-                  DatagramPacket packet = new DatagramPacket(payload, payload.length, dest_address);
+            try {
                   socket.send(packet);
                   System.out.println("Emitted sound");
-             } catch (IOException ex) {
+            } catch (IOException ex) {
                  System.out.println(ex.getMessage());
              }        
         }
     }
 
-    public static void main(String[] args) {
+    public static Instrument getInstrumentFromArgs(String[] args) {
         if (args.length < 1) {
             System.out.println("Please provide the instrument as a command-line argument.");
-            return;
+            return null;
         }
 
-        // TODO love that, but extract it to a function
-        Instrument instrument;
         try {
-            instrument = Instrument.valueOf(args[0].toUpperCase());
+            return Instrument.valueOf(args[0].toUpperCase());
         } catch (IllegalArgumentException e) {
             System.out.println("Invalid instrument. Please provide a valid instrument.");
-            return;
+            return null;
         }
+    }
+
+    public static void main(String[] args) throws IOException {
+        Instrument instrument = getInstrumentFromArgs(args);
         Musician musician = new Musician(instrument);
-        // TODO I would replace the time with this. This is a more standard way of doing it
-        //        var executor = Executors.newSingleThreadScheduledExecutor();
-        //        executor.scheduleAtFixedRate(() -> System.out.println("HEllo"), 0, 1, TimeUnit.SECONDS);
-        // or you could just have a while true loop with a Thread.sleep of 5seconds since you don't need to do anything else on the main thread
-        Timer timer = new Timer();
-        timer.schedule(musician.new PlayInstrument(), 0, 1000); // Schedule the PlayInstrument task
-        // TODO: unexpected ahah
-        Runtime.getRuntime().addShutdownHook(new Thread(timer::cancel));
+        var executor = Executors.newSingleThreadScheduledExecutor();
+        executor.scheduleAtFixedRate(musician.new PlayInstrument(), 0, 1, TimeUnit.SECONDS);
     }
 }
