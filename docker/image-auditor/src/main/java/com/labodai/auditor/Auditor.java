fa3437d281ef004c211f9c6733d08f15f81ea0e4
ansermgw <25158748+ansermgw@users.noreply.github.com>
2024-01-21 13:57:40 +0100

    add TCP server on auditor

--- docker/image-auditor/src/main/java/com/labodai/auditor/Auditor.java
+++ docker/image-auditor/src/main/java/com/labodai/auditor/Auditor.java
@@ -1,24 +1,40 @@
 package com.labodai.auditor;
 
+import com.google.gson.Gson;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.net.ServerSocket;
+import java.nio.charset.StandardCharsets;
 import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
 
 class Auditor {
+    private static final Gson gson = new Gson();
+
+    private static void handleAuditorClientRequest(MusicianState state, ServerSocket serverSocket) throws IOException {
+        try (var socket = serverSocket.accept();
+             var out = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream(), StandardCharsets.UTF_8))) {
+
+            out.write(gson.toJson(state.getMusicians()));
+        }
+    }
+
     public static void main(String[] args) {
         var state = new MusicianState();
         var listener = new MusicianListener(state);
 
-        try (var executor = Executors.newVirtualThreadPerTaskExecutor()) {
+        try (var executor = Executors.newVirtualThreadPerTaskExecutor();
+             var socket = new ServerSocket(2205)) {
             // Create a UDP client listening for musician messages
             executor.submit(listener);
 
-
-            // Musician expiration thread
-            // TODO musician API
-
-
-            executor.awaitTermination(100000, TimeUnit.DAYS);
-        } catch (InterruptedException e) {
+            // Serve active musician list on TCP socket
+            // TODO make implementation a multithreading server based on virtual thread
+            while (true) {
+                handleAuditorClientRequest(state, socket);
+            }
+        } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
